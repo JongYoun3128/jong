3c2ae76aed29eefa420cab3fb793076949eca5e4
JongYoun3128 <whddbs3128@gmail.com>
2025-05-29 16:49:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="test">
+    <h2 className='text-red-500'>클릭하면 바로가기</h2>
+    <button
+    className='tour-btn'
+    ><a href="https://home-zjqq.vercel.app">바로가기</a></button>
+    <button
+    className='tour-btn'
+    ><a href="https://homedddd.vercel.app">바로가기</a></button>
+    </div>
+    
+  );
+}
+
+export default App;
